2e08f49aa9217bb783309a49a65d9d5a3df5ebaf
Manuel Alvarez <zuma3434@gmail.com>
2015-11-19 16:47:23 -0600

    first commit added backend arrays to front end

--- pop_Up.js
+++ pop_Up.js
@@ -26,9 +26,17 @@ $(function() {
 
 });
 
+
+
+
+
 $.fn.slideFadeToggle = function(easing, callback) {
-  return this.animate({opacity: 'toggle', height: 'toggle'}, 'fast', easing, callback);
+  return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
 };
+
+
+
+
 
 /*$('form#newEmployee').on('submit', 'button', function(){
   var submitVal = $('#newEmployee'.value)
--- script.js
+++ script.js
@@ -1,74 +1,169 @@
-$(function() {
+/**
+ * Employee representation.
+ */
+function Employee(firstName, lastName, employeeNum, employeeTitle, lastReview, employeeSalary){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.employeeNum = employeeNum;
+  this.employeeTitle = employeeTitle;
+  this.lastReview = lastReview;
+  this.employeeSalary = employeeSalary;
+}
 
-  $('form#newEmployee').on('submit', function(event) {
+/**
+ * Employee list no unique id number function
+ */
 
-    var data = $(this).serializeArray();
+ function EmployeeList(){
+       list = [];
+       /**
+     	 * Get a copy of the list
+       *splice duplicates the list array at 0 index
+     	 */
+       this.getList = function(){
+         return list.splice(0);
 
-    console.log(data);
+       }
+       /**
+        * Add Employee
+        * @param an employee if employee is a instanceOf Employee constructor
+        */
+        this.addEmployee = function(employee){
+          if(numExists(employee.employeeNum)){
+            console.log("already assigned to someone");
+            return;
+          }
+          if(employee instanceof Employee){
+            list.push(employee);
+          }
+        }
 
-    /**Very long if statment to bring the object data into the DOM.
-     *The name data appends a ul to #content div.
-     *Then each data point of the array appends a li to the ul created
-     **/
-    for (var i = 0; i < data.length; i++) {
-      var elem = data[i];
+        /**
+         * Delete Employee
+         * @param employee number
+         */
+         this.removeEmployee = function(employeeNum){
+           for (var i = 0; i < list.length; i++) {
+             if (employeeNum == list[i].employeeNum) {
+               list.splice([i],1);
+               break;
+             }
+           }
+         }
 
-      if (elem.name === 'firstName') {
-        var $ul = $('<ul>');
-        $('#content').append($ul);
-        var $li = $('<li>');
-        $li.text('Name: ' + elem.value);
-        $('#content ul:last-child').append($li);
+         /**
+          * Check for employeeNum
+          * @param employee number
+          */
+          numExists = function(employeeNum){
+            return list.some(function(element, index, array){
+              return employeeNum == element.employeeNum;
+            });
+          }
 
-      }
-      if (elem.name === 'lastName') {
-        var $li = $('<li>');
-        $li.text('Last Name: ' + elem.value);
-        $('#content ul:last-child').append($li);
 
-      }
-      if (elem.name === 'employeeNum') {
-        var $li = $('<li>');
-        $li.text('Employee #: ' + elem.value);
-        $('#content ul:last-child').append($li);
+ }
 
-      }
-      if (elem.name === 'employeeTitle') {
-        var $li = $('<li>');
-        $li.text('Title: ' + elem.value);
-        $('#content ul:last-child').append($li);
+$(document).ready(function(){
+  var list = new EmployeeList();
 
-      }
-      if (elem.name === 'lastReview') {
-        var $li = $('<li>');
-        var cNum = parseInt(elem.value);
-        $li.text('Latest Employee Review: ' + elem.value);
-        $('#content ul:last-child').append($li);
-        $li.addClass('review' + cNum);
+/* add employees to the list with the addEmployee function*/
+  list.addEmployee(new Employee('Manuel', 'Alvarez', 1 , 'Me', 4, 50000));
+  list.addEmployee(new Employee('Andy', 'WIlson', 2 , 'Me', 5, 50000));
+  list.addEmployee(new Employee('Andy', 'WIlson', 3 , 'Me', 5, 50000));
 
-      }
-      /**
-      * added a delete button that is appended to the ul and deletes
-      * the entire ul its within
-      */
-      if (elem.name === 'employeeSalary') {
-        var $li = $('<li>');
-        var $delButton = $('<button type="button" class = "del" name="button">Delete</button>');
-        $li.text('Salary: ' + elem.value);
-        $('#content ul:last-child').append($li, $delButton);
-        $('#content').on('click', '.del', function() {
-          $(this).closest('ul').remove();
-        });
+    // create template for displaying employees
+ 	  var employeeTemplate = $('#employee-list').html();
 
-      }
+    // Compile the template
+    var template = Handlebars.compile(employeeTemplate);
 
-      $(this)[0].reset();
-    }
+    // Pass our data to the template
+    var compiledHtml = template({employees: list.getList()});
 
-    $('#sM').on('click', function() {
-        $('#formWindow').prop('style','none');
-      });
 
-    event.preventDefault();
-  });
+    // Add the compiled html to the page
+    $('.employees').append(compiledHtml);
 });
+
+
+
+
+
+
+
+
+// $(function() {
+//
+//   $('form#newEmployee').on('submit', function(event) {
+//
+//     var data = $(this).serializeArray();
+//
+//     console.log(data);
+//
+//     /**Very long if statment to bring the object data into the DOM.
+//      *The name data appends a ul to #content div.
+//      *Then each data point of the array appends a li to the ul created
+//      **/
+//     for (var i = 0; i < data.length; i++) {
+//       var elem = data[i];
+//
+//       if (elem.name === 'firstName') {
+//         var $ul = $('<ul>');
+//         $('#content').append($ul);
+//         var $li = $('<li>');
+//         $li.text('Name: ' + elem.value);
+//         $('#content ul:last-child').append($li);
+//
+//       }
+//       if (elem.name === 'lastName') {
+//         var $li = $('<li>');
+//         $li.text('Last Name: ' + elem.value);
+//         $('#content ul:last-child').append($li);
+//
+//       }
+//       if (elem.name === 'employeeNum') {
+//         var $li = $('<li>');
+//         $li.text('Employee #: ' + elem.value);
+//         $('#content ul:last-child').append($li);
+//
+//       }
+//       if (elem.name === 'employeeTitle') {
+//         var $li = $('<li>');
+//         $li.text('Title: ' + elem.value);
+//         $('#content ul:last-child').append($li);
+//
+//       }
+//       if (elem.name === 'lastReview') {
+//         var $li = $('<li>');
+//         var cNum = parseInt(elem.value);
+//         $li.text('Latest Employee Review: ' + elem.value);
+//         $('#content ul:last-child').append($li);
+//         $li.addClass('review' + cNum);
+//
+//       }
+//       /**
+//       * added a delete button that is appended to the ul and deletes
+//       * the entire ul its within
+//       */
+//       if (elem.name === 'employeeSalary') {
+//         var $li = $('<li>');
+//         var $delButton = $('<button type="button" class = "del" name="button">Delete</button>');
+//         $li.text('Salary: ' + elem.value);
+//         $('#content ul:last-child').append($li, $delButton);
+//         $('#content').on('click', '.del', function() {
+//           $(this).closest('ul').remove();
+//         });
+//
+//       }
+//
+//       $(this)[0].reset();
+//     }
+//
+//     $('#sM').on('click', function() {
+//         $('#formWindow').prop('style','none');
+//       });
+//
+//     event.preventDefault();
+//   });
+// });
